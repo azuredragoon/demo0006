51ca0eee6789f6cd17c37555cef7c9b10adc9f3e
azuredragoon <terencenmnpan@gmail.com>
2018-06-17 23:42:36 -0700

    Initial commit

---
+++ src/main/java/HelloResource.java
@@ -0,0 +1,19 @@
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * JAX-RS Resource class
+ * 
+ * @author tpan
+ */
+@Path("hello2")
+public class HelloResource {
+
+    @GET
+    @Produces({MediaType.TEXT_PLAIN})
+    public String hello() {
+        return "Hello world from JAX-RS!";
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+import org.glassfish.embeddable.GlassFish;
+import org.glassfish.embeddable.GlassFishProperties;
+import org.glassfish.embeddable.GlassFishRuntime;
+import org.glassfish.embeddable.archive.ScatteredArchive;
+
+import java.io.File;
+import org.glassfish.embeddable.Deployer;
+
+/**
+ * Bootstrap class
+ * 
+ * @author tpan
+ */
+public class Main{
+    public static void main(String[] args) throws Exception {
+        String port = System.getenv("PORT");
+        port = port != null ? port : "8080";
+        GlassFishProperties gfProps = new GlassFishProperties();
+        gfProps.setPort("http-listener", Integer.parseInt(port));
+
+        GlassFish glassfish = GlassFishRuntime.bootstrap().newGlassFish(gfProps);
+        glassfish.start();
+
+        File webRoot = new File("src/main/webapp");
+        File classRoot = new File("target", "classes");
+
+        Deployer deployer = glassfish.getDeployer();
+        ScatteredArchive archive = new ScatteredArchive("hello", ScatteredArchive.Type.WAR, webRoot);
+        archive.addClassPath(classRoot);
+        deployer.deploy(archive.toURI());
+    }
+}
